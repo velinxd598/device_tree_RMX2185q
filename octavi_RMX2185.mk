f245dc82b300efeba90cb98452fd054c67b90394
velinxd <153659670+velinxd@users.noreply.github.com>
2024-03-22 13:27:03 +0700

    Update and rename lineage_RMX2185.mk to octavi_RMX2185.mk

--- octavi_RMX2185.mk
+++ octavi_RMX2185.mk
@@ -22,14 +22,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2185/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_RMX2185
+PRODUCT_NAME := octavi_RMX2185
 PRODUCT_DEVICE := RMX2185
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G35 Series
 PRODUCT_MANUFACTURER := realme
+
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_USES_BLUR := true
 
 TARGET_BOOT_ANIMATION_RES := 720
 
@@ -37,3 +40,10 @@ PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 # Time
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+# Official
+OCTAVI_BUILD_TYPE := Community
+OCTAVI_MAINTAINER := VelinXD
+
+# Gapps Build
+WITH_GAPPS := false
